5987fcc7fcc939df97712a9d3b32323514ea7453
MarioLetsche <mario.letsche@icloud.com>
2025-03-03 09:45:39 +0100

    Basic Spring Boot Interface for car rental.

--- src/test/java/de/superrental/carrentalinterface/CarRentalInterfaceApplicationTest.java
+++ src/test/java/de/superrental/carrentalinterface/CarRentalInterfaceApplicationTest.java
@@ -3,8 +3,6 @@ package de.superrental.carrentalinterface;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
 class CarRentalInterfaceApplicationTest {
     @Test
